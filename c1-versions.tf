f824111f8d1d270c46f93f9ec33733fb13ac89fb
Piyushkr44 <106644687+Piyushkr44@users.noreply.github.com>
2022-07-10 16:30:25 +0530

    Update c1-versions.tf

--- c1-versions.tf
+++ c1-versions.tf
@@ -21,16 +21,12 @@ data "azurerm_key_vault" "terrakv" {
   resource_group_name = "cicd" // resourceGroup
 }
 
-data "azurerm_key_vault_secret" "terraformjenkins" {
-name = "terraformjenkins" // Name of secret
-key_vault_id = data.azurerm_key_vault.terrakv.id
-}
 # Provider Block
 provider "azurerm" {
  features {}   
  subscription_id = "b4fd38ca-930c-4e35-a642-d7f7075f428e"
   client_id       = "d7400eae-a48a-4710-9256-d5f16cc12437"
-  client_secret   = data.azurerm_key_vault_secret.terraformjenkins.value
+  client_secret   = "EOzYwgqbkZVLtawjvfF5qYXk.f3EdZUrbS"
   tenant_id       = "961e4a55-c338-498d-a310-544a2b8c027e"       
 }
 
